f48ed70e1293f110bb114608b3768798b9ab7527
MerManahan <mermanahan@gmail.com>
2021-07-12 18:05:55 -0400

    first commit

---
+++ target/run/NBA_Data/models/example/my_first_dbt_mode_NBA_Datal.sql
@@ -0,0 +1,29 @@
+
+
+  create  table "NBA_Data"."public"."my_first_dbt_mode_NBA_Datal__dbt_tmp"
+  as (
+    /*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+
+
+with source_data as (
+
+    select * from public.all_years_data
+
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
+  );
